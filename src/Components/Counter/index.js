c43ccfee8bdc10a82dd46feebf689aebe87383ba
Benjamin LOUSTAUNAU <benjamin.loustaunau@cdbdx.biz>
2023-06-06 10:41:02 +0200

    add inc / dec

--- src/Components/Counter/index.js
+++ src/Components/Counter/index.js
@@ -1,18 +1,13 @@
 import React from "react";
+import { useCounter } from "react-use";
 
 export default function Counter({ startValue = 0, step = 1 }) {
-  const [currentValue, updateValue] = React.useState(startValue);
+  const [currentValue, { inc, dec }] = useCounter(startValue, null, startValue);
   return (
     <>
       <p>{currentValue}</p>
-      <button
-        onClick={() => {
-          console.log("click");
-          updateValue(currentValue + step);
-        }}
-      >
-        Click me
-      </button>
+      <button onClick={() => dec(step)}>- {step}</button>
+      <button onClick={() => inc(step)}>+ {step}</button>
     </>
   );
 }
